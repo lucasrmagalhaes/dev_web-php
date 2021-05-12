7be04e23fd3388411d65210a588e467697528cd2
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-12 00:31:55 -0300

    Conclusão do Módulo

--- index.php
+++ index.php
@@ -25,82 +25,9 @@
                     <h3>Módulo 01</h3>
                     <ul>
                         <li><a href="curso-php/exercicio.php?dir=teste&file=teste">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
                     </ul>
                 </div>
 
-                <div class="modulo vermelho">
-                    <h3>Módulo 02</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo azul">
-                    <h3>Módulo 03</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo roxo">
-                    <h3>Módulo 04</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo laranja">
-                    <h3>Módulo 05</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo verde-escuro">
-                    <h3>Módulo 06</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo vermelho-escuro">
-                    <h3>Módulo 07</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo azul-escuro">
-                    <h3>Módulo 08</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
-
-                <div class="modulo roxo-escuro">
-                    <h3>Módulo 09</h3>
-                    <ul>
-                        <li><a href="#">Exercício A</a></li>
-                        <li><a href="#">Exercício B</a></li>
-                        <li><a href="#">Exercício C</a></li>
-                    </ul>
-                </div>
             </nav>
         </div>
     </main>
