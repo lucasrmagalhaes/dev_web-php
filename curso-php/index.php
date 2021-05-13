2af554fb01f0647b8f591edb2ebfac7cd22d4985
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-12 23:12:58 -0300

    Tipo Float

--- curso-php/index.php
+++ curso-php/index.php
@@ -36,6 +36,7 @@
                     <h3>Tipos</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=tipos&file=int">Tipo Inteiro</a></li>
+                        <li><a href="exercicio.php?dir=tipos&file=float">Tipo Float</a></li>
                     </ul>
                 </div>
 
