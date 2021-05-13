031aeaec9365b3acb795735769d90b9f6ef71709
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-12 22:23:57 -0300

    Comentários PHP

--- curso-php/index.php
+++ curso-php/index.php
@@ -27,6 +27,7 @@
                         <li><a href="exercicio.php?dir=basico&file=ola">Olá PHP</a></li>
                         <li><a href="exercicio.php?dir=basico&file=html">Integração HTML</a></li>
                         <li><a href="exercicio.php?dir=basico&file=css">Integração CSS</a></li>
+                        <li><a href="exercicio.php?dir=basico&file=comentarios">Comentários PHP</a></li>
                     </ul>
                 </div>
 
