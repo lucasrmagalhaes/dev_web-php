d680b2516c9f09faadcb4fb6d3040c5d5bc60d98
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-13 15:58:24 -0300

    Desafio String

---
+++ curso-php/tipos/desafio_string.php
@@ -0,0 +1,11 @@
+<div class="titulo">Desafio String</div>
+
+<?php
+/**
+ * Enunciado:
+ *  Avaliando os métodos da documentação da string,
+ * qual o método que a posição do texto 'abc' na
+ * string '!AbcaBcabc' retorne 1.
+ */
+
+ 
