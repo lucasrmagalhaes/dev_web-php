5b50a101ac831eaf0f6af673142feacba8949b67
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-16 20:42:10 -0300

    Variáveis Básico

--- curso-php/variaveis/basico.php
+++ curso-php/variaveis/basico.php
@@ -23,3 +23,16 @@ echo '<br>' . $variavel; // 10
 
 $variavel = "Agora sou uma string!";
 echo '<br>' . $variavel; // "Agora sou uma string!";
+
+// Nomes de variável
+$var = 'valida';
+$var2 = 'valida';
+$VAR3 = 'valida';
+$_var_4 = 'valida';
+$vâr5 = 'valida'; // evitar
+// $6var = 'invalida';
+// $%var7 = 'invalida';
+// $var8% = 'invalida';
+
+echo '<br>' . $_SERVER;
+var_dump($_SERVER["HTTP_HOST"]); // Variáveis pré-definidas: https://www.php.net/manual/pt_BR/reserved.variables.php
