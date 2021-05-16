344342511d9d9113bf5844a1f4a8ace54f9363c0
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-16 20:53:42 -0300

    Desafio Equação

--- curso-php/index.php
+++ curso-php/index.php
@@ -50,6 +50,7 @@
                     <h3>Variáveis</h3>
                     <ul>
                         <li><a href="exercicio.php?dir=variaveis&file=basico">Variáveis</a></li>
+                        <li><a href="exercicio.php?dir=variaveis&file=desafio_equacao">Desafio Equação</a></li>
                     </ul>
                 </div>
 
