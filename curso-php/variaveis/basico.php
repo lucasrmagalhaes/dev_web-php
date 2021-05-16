75cf12e278c6cb7799fe965036296d5ef91d158c
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-16 20:29:03 -0300

    Variáveis 1

--- curso-php/variaveis/basico.php
+++ curso-php/variaveis/basico.php
@@ -1,2 +1,25 @@
 <div class="titulo">Variáveis</div>
 
+<?php
+$numeroA = 13;
+echo $numeroA, '<br>';
+var_dump($numeroA);
+
+echo '<br>';
+$a = 3;
+$b = 16;
+$soma = $a + $b;
+echo $somaDosNumeros;
+
+echo '<br>';
+echo isset($somaDosNumeros); // 1
+
+unset($somaDosNumeros);
+echo '<br>';
+var_dump($somaDosNumeros); // null
+
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = "Agora sou uma string!";
+echo '<br>' . $variavel;
