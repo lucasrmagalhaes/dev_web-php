1aabb254053e7a729007559dd902400b46ebe8bd
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-16 21:41:24 -0300

    Desafio Variáveis

--- curso-php/index.php
+++ curso-php/index.php
@@ -54,6 +54,7 @@
                         <li><a href="exercicio.php?dir=variaveis&file=atribuicoes">Atribuições</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=interpolacao">Interpolação</a></li>
                         <li><a href="exercicio.php?dir=variaveis&file=variaveis_variaveis">Variáveis Variáveis</a></li>
+                        <li><a href="exercicio.php?dir=variaveis&file=desafio_variaveis">Desafio Variáveis</a></li>
                     </ul>
                 </div>
 
