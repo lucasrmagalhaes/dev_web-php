1933477ddc320b027fd71d642cf94326603433e7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-20 20:00:25 -0300

    Operadores Relacionais parte 2

--- curso-php/controle/operadores_relacionais.php
+++ curso-php/controle/operadores_relacionais.php
@@ -25,3 +25,25 @@ if($idade < 18) {
 } else {
     echo "Terceira idade = $idade anos!";
 }
+
+echo '<p>Spaceship</p><hr>';
+var_dump(500 <=> 3); // 1
+var_dump(50 <=> 50); // 0
+var_dump(5 <=> 50); // -1
+
+echo '<p>Valores pode ser V ou F</p><hr>';
+var_dump(!!5); // true
+var_dump(!!0); // false
+var_dump(!!""); // false 
+var_dump(!!" "); // true
+?>
+
+<style>
+p {
+    margin-bottom: 0px;
+}
+
+hr {
+    margin-top: 0px;
+}
+</style>
