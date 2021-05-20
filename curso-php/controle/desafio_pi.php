91e91a747c268fce3a106ab16fa307d434591a69
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-20 20:12:53 -0300

    Solução Desafio PI

--- curso-php/controle/desafio_pi.php
+++ curso-php/controle/desafio_pi.php
@@ -12,3 +12,17 @@ if($pi === pi()) {
 
 // Operador Relacional
 $piErrado = 2.8;
+
+// Resposta
+echo '<br>' . ($pi - pi());
+echo '<br>' . ($pi - $piErrado);
+
+if($pi - pi() <= 0.01) {
+    echo '<br>Praticamente iguais!';
+}
+
+if($pi - $piErrado <= 0.01) {
+    echo '<br>Praticamente iguais!';
+} else {
+    echo '<br>Valor errado!';
+}
