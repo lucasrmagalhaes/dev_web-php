f33f3df09563004bf64c4f16dbaf89e2b7fb90ad
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-20 19:41:56 -0300

    If Else parte 2

--- curso-php/controle/if_else.php
+++ curso-php/controle/if_else.php
@@ -4,3 +4,23 @@
 if(true) {
     echo "Serei impresso?<br>";
 }
+
+if(false) {
+    echo "Verdadeiro - Parte A<br>";
+    echo "Verdadeiro - Parte B<br>";
+} else {
+    echo "Falso - Parte A<br>";
+    echo "Falso - Parte B<br>";
+}
+
+if(false) {
+    echo "Passo A<br>";
+} else if(false) {
+    echo "Passo B<br>";
+} elseif(true) {
+    echo "Passo C<br>";
+} else {
+    echo "Último Passo<br>";
+}
+
+echo "Fim<br>";
