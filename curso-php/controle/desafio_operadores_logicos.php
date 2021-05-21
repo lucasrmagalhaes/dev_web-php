2ce3d67d44ebd95cfa830a2314ff1579fe3499ee
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-21 00:51:02 -0300

    Solução

--- curso-php/controle/desafio_operadores_logicos.php
+++ curso-php/controle/desafio_operadores_logicos.php
@@ -32,5 +32,37 @@
 </style>
 
 <?php
-var_dump(!!$_POST['t1']);
-var_dump(!!$_POST['t2']);
+
+if(isset($_POST['t1']) && isset($_POST['t2'])) {
+    $t1 = $_POST['t1'] === '1';
+    $t2 = !!$_POST['t2'];
+    $tv = '';
+    $sorvete = false;
+
+    if($t1 && $t2) {
+        $tv = '50"';
+    } elseif($t1 xor $t2) {
+        $tv = '32"';
+    }
+
+    if($t1 or $t2) {
+        $sorvete = true;
+    }
+
+    if($tv) {
+        $resultado = "Vamos comprar uma TV de $tv";
+    } else {
+        $resultado = "Sem TV dessa vez : (";
+    }
+
+    $saudavel = !$sorvete;
+
+    if(!$sorvete) {
+        $resultado .= '<br>Estamos mais saudáveis!';
+    } else {
+        $resultado .= '<br>Sorvete liberado \o/';
+    }
+
+    echo "<p>$resultado</p>";
+}
+
