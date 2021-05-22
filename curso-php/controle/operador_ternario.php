16fc5f88c3d624ece281dd378f84c762ae171b91
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-22 16:20:08 -0300

    Operador Ternário

---
+++ curso-php/controle/operador_ternario.php
@@ -0,0 +1,24 @@
+<div class="php">Operador Ternário</div>
+
+<?php
+$idade = 70;
+$status;
+
+if($idade >= 18) {
+    $status = 'Maior de idade';
+} else {
+    $status = 'Menor de idade';
+}
+
+echo "$status<br>";
+
+$idade = 17;
+$status = $idade >= 18 ? 'Maior de idade' : 'Menor de idade';
+
+echo "$status<br>";
+
+$idade = 12;
+$status = $idade >= 18 ? $idade >= 65 ? 'Aposentado' : 'Maior de idade' : 'Menor de idade';
+
+echo "$status<br>";
+
--- curso-php/index.php
+++ curso-php/index.php
@@ -36,6 +36,7 @@
                         <li><a href="exercicio.php?dir=controle&file=desafio_pi">Desafio PI</a></li>
                         <li><a href="exercicio.php?dir=controle&file=operadores_logicos">Operadores Lógicos</a></li>
                         <li><a href="exercicio.php?dir=controle&file=desafio_operadores_logicos">Desafio Operadores Lógicos</a></li>
+                        <li><a href="exercicio.php?dir=controle&file=operador_ternario">Operador Ternário</a></li>
                     </ul>
                 </div>
 
