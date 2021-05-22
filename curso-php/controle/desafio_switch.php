3dd3ecf03d687c1448963c4333c7b31d495b7039
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-22 20:19:19 -0300

    Solução

--- curso-php/controle/desafio_switch.php
+++ curso-php/controle/desafio_switch.php
@@ -4,9 +4,9 @@
     <input type="text" name="param">
     <select name="conversao" id="conversao">
         <option value="km-milha">Km > Milha</option>
-        <option value="km-milha">Milha > Km</option>
-        <option value="km-milha">Metros > Km</option>
-        <option value="km-milha">Km > Metros</option>
+        <option value="milha-km">Milha > Km</option>
+        <option value="metro-km">Metros > Km</option>
+        <option value="km-metro">Km > Metros</option>
     </select>
     <button>Calcular</button>
 </form>
@@ -18,5 +18,30 @@
 </style>
 
 <?php
-echo $_POST['param'] . '<br>';
-echo $_POST['conversao'];
+const FATOR_KM_MILHA = 0.621371;
+const FATOR_METRO_KM = 1000;
+
+$param = $_POST['param'] ?? 0;
+
+switch ($_POST['conversao']) {
+    case 'km-milha':
+        $distancia = $param * FATOR_KM_MILHA;
+        $mensagem = "$param Km(s) = $distancia Milha(s)";
+        break;
+    case 'milha-km':
+        $distancia = $param / FATOR_KM_MILHA;
+        $mensagem = "$param Milha(s) = $distancia Km(s)";
+        break;
+    case 'metro-km':
+        $distancia = $param / FATOR_METRO_KM;
+        $mensagem = "$param Metro(s) = $distancia Km(s)";
+        break;
+    case 'km-metro':
+        $distancia = $param * FATOR_METRO_KM;
+        $mensagem = "$param Km(s) = $distancia Metro(s)";
+        break;
+    default:
+        $mensagem = "Nenhum valor calculado";
+}
+
+echo "<p>$mensagem</p>";
