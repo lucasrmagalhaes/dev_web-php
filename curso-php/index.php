0c8f86b1cfa461dc4f08870086fb8d43601120a3
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-23 00:28:33 -0300

    Multidimensionais

--- curso-php/index.php
+++ curso-php/index.php
@@ -37,6 +37,8 @@
                         <li><a href="exercicio.php?dir=array&file=desafio_meses">Desafio Meses</a></li>
                         <li><a href="exercicio.php?dir=array&file=operacoes">Operações</a></li>
                         <li><a href="exercicio.php?dir=array&file=desafio_sorteio">Desafio Sorteio</a></li>
+                        <li><a href="exercicio.php?dir=array&file=multi">Multidimensionais</a></li>
+                        <li><a href="exercicio.php?dir=array&file="></a></li>
                         <li><a href="exercicio.php?dir=array&file="></a></li>
                     </ul>
                 </div>
