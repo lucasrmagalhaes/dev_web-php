3a67bc430a78bb8a7900a0c684725edb671e7f7a
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-05-23 00:46:55 -0300

    GET

--- curso-php/index.php
+++ curso-php/index.php
@@ -39,6 +39,7 @@
                         <li><a href="exercicio.php?dir=array&file=desafio_sorteio">Desafio Sorteio</a></li>
                         <li><a href="exercicio.php?dir=array&file=multi">Multidimensionais</a></li>
                         <li><a href="exercicio.php?dir=array&file=constantes">Arrays Constantes</a></li>
+                        <li><a href="exercicio.php?dir=array&file=get">$_GET</a></li>
                         <li><a href="exercicio.php?dir=array&file="></a></li>
                     </ul>
                 </div>
