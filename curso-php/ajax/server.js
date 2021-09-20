adf746e272a26ce258d1ac93d1d6b8b3eca2c35d
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-09-20 14:20:48 -0300

    XMLHttpRequest 01

--- curso-php/ajax/server.js
+++ curso-php/ajax/server.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("."));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.get("/teste", (req, res) => res.send("OK"));
+// app.get("/teste", (req, res) => res.send(new Date()));
 app.listen(8080, () => console.log("Executando..."));
