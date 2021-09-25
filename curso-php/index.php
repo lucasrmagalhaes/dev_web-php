4f87b15b24bf1b07bf6de4486df579c54665731d
Lucas Magalhães <lucas.magalhaes@oabrs.org.br>
2021-09-25 16:11:51 -0300

    Excluir Registro 01

--- curso-php/index.php
+++ curso-php/index.php
@@ -28,6 +28,7 @@
             <li><a href="exercicio.php?dir=db&file=criar_tabela">Criar Tabela</a></li>
             <li><a href="exercicio.php?dir=db&file=inserir_1">Inserir Registro #01</a></li>
             <li><a href="exercicio.php?dir=db&file=consultar">Consultar Registros</a></li>
+            <li><a href="exercicio.php?dir=db&file=excluir_1">Excluir Registro #01</a></li>
           </ul>
         </div>
 
