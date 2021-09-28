bb751359382f72efe351886843ebece62efbcac3
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-09-28 20:36:01 -0300

    XMLHttpRequest 02

--- curso-php/ajax/server.js
+++ curso-php/ajax/server.js
@@ -1,10 +1,33 @@
-const bodyParser = require("body-parser");
-const express = require("express");
-const app = express();
+const bodyParser = require("body-parser")
+const express = require("express")
+const app = express()
 
-app.use(express.static("."));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.static("."))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-// app.get("/teste", (req, res) => res.send(new Date()));
-app.listen(8080, () => console.log("Executando..."));
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './upload')
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`)
+  }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      return res.end('Ocorreu um erro.')
+    }
+
+    res.end('Concluído com sucesso.')
+  })
+})
+
+// app.get("/teste", (req, res) => res.send(new Date()))
+app.listen(8080, () => console.log("Executando..."))
