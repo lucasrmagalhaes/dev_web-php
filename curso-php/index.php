bfe40cce92af0398622833cac8b8aee137bb3149
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-11-13 12:09:12 -0300

    Include Once/Require Once

--- curso-php/index.php
+++ curso-php/index.php
@@ -41,6 +41,7 @@
             <li><a href="exercicio.php?dir=includes&file=include_funcao">Include Função</a></li>
             <li><a href="exercicio.php?dir=includes&file=include_require">Include vs Require</a></li>
             <li><a href="exercicio.php?dir=includes&file=require_return">Require & Return</a></li>
+            <li><a href="exercicio.php?dir=includes&file=include_once">Include Once</a></li>
           </ul>
         </div>
 
