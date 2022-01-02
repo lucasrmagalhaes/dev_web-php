b9c5298ad0eb393b8ee262064cc1581e2098ab95
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-02 14:19:57 -0300

    Desafio For - Resposta

--- curso-php/repeticoes/desafio_for.php
+++ curso-php/repeticoes/desafio_for.php
@@ -12,3 +12,21 @@ Usar o for...
 1 - Pode usar incremento $i++
 2 - Não pode usar incremento $i++
 -->
+
+<?php
+$impressao = '';
+
+for ($cont = 1; $cont <= 5; $cont++) {
+    $impressao .= '#';
+    echo "$impressao <br>";
+}
+
+echo '<hr>';
+
+for (
+    $impressao2 = '#'; 
+    $impressao2 !== '######';
+    $impressao2 .= '#'
+) {
+    echo "$impressao2 <br>";
+}
