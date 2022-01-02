605205689331f5742f4fb7e39c07a6e0589c15db
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-02 14:47:05 -0300

    Desafio Impressão

--- curso-php/menu.php
+++ curso-php/menu.php
@@ -32,6 +32,7 @@
             <li><a href="exercicio.php?dir=repeticoes&file=desafio_for">Desafio For</a></li>
             <li><a href="exercicio.php?dir=repeticoes&file=foreach">Foreach</a></li>
             <li><a href="exercicio.php?dir=repeticoes&file=break_continue">Break/Continue</a></li>
+            <li><a href="exercicio.php?dir=repeticoes&file=desafio_impressao">Desafio Impressão</a></li>
         </ul>
     </div>
 
---
+++ curso-php/repeticoes/desafio_impressao.php
@@ -0,0 +1,18 @@
+<div class="titulo">Desafio Impressão</div>
+
+<!--
+    Enunciado: 
+    - Imprima apenas os valores do array que contém inidice par;
+    - Resolver com for e foreach; e
+    - Valores esperados: AAA, CCC e EEE.
+-->
+
+<?php
+$array = [
+    "AAA",
+    "BBB",
+    "CCC",
+    "DDD",
+    "EEE",
+    "FFF"
+];
