66035ea93c44ef3eec9968f0200246201e69b2fe
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-01-02 15:00:35 -0300

    While/Do While

--- curso-php/menu.php
+++ curso-php/menu.php
@@ -33,6 +33,7 @@
             <li><a href="exercicio.php?dir=repeticoes&file=foreach">Foreach</a></li>
             <li><a href="exercicio.php?dir=repeticoes&file=break_continue">Break/Continue</a></li>
             <li><a href="exercicio.php?dir=repeticoes&file=desafio_impressao">Desafio Impressão</a></li>
+            <li><a href="exercicio.php?dir=repeticoes&file=while">While/Do While</a></li>
         </ul>
     </div>
 
